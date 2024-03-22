38a896db6d8895c0dae46bceccb6b1ec25a78f3f
Pablo <pablocode010@gmail.com>
2024-03-21 19:10:52 -0600

    feat: add /ping route to test

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const router = require("./routes/router");
 
-app.listen(PORT, ()=> {
-    console.log(`http://127.0.0.1:${PORT}/`);
-})
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`http://127.0.0.1:${PORT}/`);
+});
