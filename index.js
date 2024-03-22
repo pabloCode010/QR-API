5ec83f7a03a3c866dc7fc1419e8f4d5b1a4685f4
Pablo <pablocode010@gmail.com>
2024-03-21 19:12:20 -0600

    feat: add morgan logger

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 const router = require("./routes/router");
+const morgan = require("morgan");
 
+app.use(morgan("dev"));
 app.use(router);
 
 app.listen(PORT, () => {
